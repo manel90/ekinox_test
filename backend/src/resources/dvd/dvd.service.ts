a3759445bb92e5ed74277d60826ca698140cbf66
manel <manel_zarraa@hotmail.fr>
2024-06-05 17:42:47 +0200

    fix(DVD):fixed calculate price

--- backend/src/resources/dvd/dvd.service.ts
+++ backend/src/resources/dvd/dvd.service.ts
@@ -4,25 +4,25 @@ import {BTTF_PRICE, BTTF_TITLES, OTHER_MOVIE_PRICE} from "../../utils/constant";
 export class DvdService {
 
 
-     constructor() {
-     }
+    constructor() {
+    }
 
     /**
      * post  calculate '
      * @param {string []} dvd
      */
 
-      calculateTotalPrice= (list: string[]) : number=>{
+    calculateTotalPrice = (list: string[]) => {
 
         const items = list;
-         if (!list.length) return 0 ;
+        if (!list.length) return 0;
 
         // Filter the items to get only "Back to the Future"
         const bttfItems: string[] = items.filter(item => BTTF_TITLES.includes(item));
         // Filter the items to get only other DVD
         const otherItems: string[] = items.filter(item => !BTTF_TITLES.includes(item));
 
-        const bttfCount = bttfItems.length;
+        const bttfCount = new Set(bttfItems).size;
         //calculate discount
         let discount = 0;
         if (bttfCount === 2) {
@@ -30,13 +30,13 @@ export class DvdService {
         } else if (bttfCount >= 3) {
             discount = 0.20; // 20% discount
         }
-       // Calculate the total price  without discount
+
+        // Calculate the total price  without discount
         const bttfTotal = bttfItems.length * BTTF_PRICE;
         const otherTotal = otherItems.length * OTHER_MOVIE_PRICE;
         //Calculate the total price after applying the discount
         const totalAfterDiscount = (bttfTotal * (1 - discount)) + otherTotal;
-
-       return parseFloat(totalAfterDiscount.toFixed(2));
+        return parseFloat(totalAfterDiscount.toFixed(2));
     }
 
 
